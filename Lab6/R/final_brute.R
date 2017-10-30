3527abc7a0a237eea2037dbd6cfddaafa21bd276
amichu22 <amishs93@gmail.com>
2017-10-30 16:36:41 +0200

    Final

--- Lab6/R/final_brute.R
+++ Lab6/R/final_brute.R
@@ -1,106 +1,182 @@
+library(Rcpp)
 
-#' Brute Force Algorithm 
+
+
+#' Brute Force Algorithm in R
+
 #'
-#' Knapsack problem is solved using brute force algorithm.
+
+#' Knapsack problem solution using Brute Force Algorithm in R.
+
+#' @param x Data.frame containing values and weights
+
+#' @param W Highest capacity
+
+#' @param fast logical argument for running Rcpp code.
+
 #'
-#' @param x is a data frame containing weights and values of the elements.
-#' @param W is the highest weight capacity.
-#' 
-#'
-#' @return output is the maximum value the knapsack can hold for the given weight capacity
+
+#' @return Returns maximum knapsack value and respective elements
+
 #' @export
+
 #'
-#' @examples brute_force(x = knapsack_objects[1:5,], W = 150)
-#' 
+
+#' @examples brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
+
+#'           brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500,fast=TRUE)
+
 #' @references \href{https://en.wikipedia.org/wiki/Knapsack_problem}{Dynamic Algorithm}
+
 #'  
 
-
-
-
-
-
-
-
-
-brute_force_knapsack<-function(x,W)
-{
-  stopifnot(is.data.frame(x)== TRUE && is.numeric(W)== TRUE)  
-  stopifnot(x[[1]]>0 && x[[2]]>0 && W>0)
+brute_force_knapsack <- function(x,W,fast=FALSE){
   
-    A <- rep(FALSE, n)
-    B <- rep(FALSE, n)
-    n<-nrow(x)
-    highest_value<-0
-    corresponding_elements<-c()
-    highest_value1<-0
-    corresponding_elements1<-c()
-    for (i in 1:(2^n))
-    {
-      j<- 1
-      tw<-0
-      tv<-0
-      
-      while (A[j]!=FALSE && j < n)
-      {
-        A[j]<-FALSE
-        j <- j+1
-      }
-      A[j]<-TRUE
-      for(k in 1:n)
-      {
-        if(A[k] == TRUE)
-        {
-          tw<-tw+x$w[k]
-          tv<-tv+x$v[k]
-        }
-      }
-      if((tv > highest_value )&& (tw <= W))
-      {
-        highest_value<-tv
-        corresponding_elements<-A
-      }
-      
-      
-      
-      j1<- n
-      tw1<-0
-      tv1<-0
-      
-      while (B[j1]!=FALSE && j1 > 0)
-      {
-        B[j1]<-FALSE
-        j1<- j1-1
-      }
-      B[j1]<-TRUE
-      for(k in 1:n)
-      {
-        if(B[k] == TRUE)
-        {
-          tw1<-tw1+x$w[k]
-          tv1<-tv1+x$v[k]
-        }
-      }
-      if((tv1 > highest_value1 )&& (tw1 <= W))
-      {
-        highest_value1<-tv1
-        corresponding_elements1<-B
-      }
-      
-     
-      
-      
-      
-      
-    }
-    if(tw1<tw)
-      return(list(value=highest_value, elements=(1:n)[corresponding_elements]))
-    else
-      return(list(value=highest_value, elements=(1:n)[corresponding_elements]))
+  
+  
+  #whether the inputs are non-negative interger values.
+  
+  
+  
+  if(is.data.frame(x) && is.numeric(W)) {
     
+    if(x[[1]]>0 && x[[2]]>0 && W>0){
+      
+      n<-nrow(x)
+      
+      weights<-x[[1]]
+      
+      values<-x[[2]]
+      
+      Capacity<-W
+      
+      bestValue<- 0
+      
+      bestChoice<-c()
+      
+      bestWeight<-0
+      
+      A<- rep(0,n)
+      
+      
+      
+      for (i in 1:(2^n))
+        
+      {
+        
+        
+        
+        j<-n
+        
+        tempWeight<-0
+        
+        tempValue<-0
+        
+        while(A[j]!= 0 && j>0){
+          
+          A[j]<-0
+          
+          j<- j-1
+          
+        }
+        
+        
+        
+        A[j]<- 1
+        
+        
+        
+        for (k in 1:n){
+          
+          if(A[k] == 1) 
+            
+          {
+            
+            tempWeight<-tempWeight+ weights[k]
+            
+            tempValue<-tempValue+values[k]
+            
+          }
+          
+        }
+        
+        
+        
+        if (fast==FALSE){
+          
+          if((tempValue > bestValue) && (tempWeight<= Capacity)){
+            
+            bestValue<-tempValue
+            
+            bestWeight<-tempWeight
+            
+            bestChoice<-which(A==1)   
+            
+          }
+          
+        }
+        
+        else
+          
+        {
+          
+          library(Rcpp)
+          
+          Rcpp::cppFunction(' int abc(int tempValue, int bestValue, int tempWeight,
+                            
+                            int Capacity, int bestWeight){
+                            
+                            
+                            
+                            if((tempValue > bestValue) && (tempWeight<= Capacity)){
+                            
+                            bestValue=tempValue;
+                            
+                            bestWeight=tempWeight;
+                            
+                            
+                            
+                            
+                            
+                            }
+                            
+                            return bestValue;
+                            
+                            
+                            
+                            } ')
+
+          bestValue<-abc(tempValue,bestValue,tempWeight,
+                         
+                         Capacity,bestWeight)
+          
+        }
+        
+        
+        
+      }
+      
+      
+      
+      
+      
+      
+      
+      return(list(value=round(bestValue), elements=bestChoice))
+      
+      
+      
+  }
     
+    else stop("Data is not positive ! \n",call. = FALSE)
     
 }
+  
+  else
+    
+    stop("Data is not numeric or not dataframe ! \n",call. = FALSE)
+  
+  }
 set.seed(42)
 n <- 2000 
 knapsack_objects <-data.frame( w=sample(1:4000, size = n, replace = TRUE), v=runif(n = n, 0, 10000) )
